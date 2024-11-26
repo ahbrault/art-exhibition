89fe7c466e6577152c557cfcf8ffbd7fbc8ad9af
Aurélien Brault <ahbrault@gmail.com>
2024-11-26 20:22:28 +0100

    Add auto play on scroll

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { artworks } from '@/data';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import AudioPlayer from '@/components/AudioPlayer';
-import { IoPlay } from 'react-icons/io5';
 import {
   LiaLongArrowAltLeftSolid,
   LiaLongArrowAltRightSolid,
@@ -20,27 +19,6 @@ const WorkPreview = () => {
 
   const artwork = artworks.find((art) => art.slug === slug);
   const currentIndex = artworks.findIndex((art) => art.slug === slug);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    if (!isLoaded) {
-      const body = document.body;
-      body.style.overflow = 'hidden';
-    }
-  }, [isLoaded]);
-
-  useEffect(() => {
-    const body = document.body;
-    if (!isLoaded) {
-      body.style.overflow = 'hidden';
-    } else {
-      body.style.overflow = '';
-    }
-
-    return () => {
-      body.style.overflow = '';
-    };
-  }, [isLoaded]);
 
   if (!artwork) {
     setTimeout(() => {
@@ -55,145 +33,113 @@ const WorkPreview = () => {
     );
   }
 
-  const Loader = () => {
-    return (
-      <div>
-        <div className='overflow-hidden'>
-          <div onClick={() => setIsLoaded(true)} className='cursor-pointer'>
-            <div className='fixed inset-0 z-30 flex h-full w-full items-center justify-center'>
+  return (
+    <Section ref={contentRef}>
+      <AudioPlayer audioSrc={artwork.audioPath} />
+      <div className='text-center'>
+        <h2 className='animate-fade-in mb-0 break-words text-4xl uppercase opacity-0 md:text-7xl'>
+          {artwork.title}
+        </h2>
+        <h3 className='animate-fade-in taviraj text-3xl uppercase italic opacity-0 md:text-5xl'>
+          {artwork.frequency}
+        </h3>
+        <p className='animate-fade-in mb-0 block text-lg uppercase opacity-0 md:text-2xl lg:hidden'>
+          {artwork.size}
+          <br /> {artwork.paintType}
+        </p>
+      </div>
+
+      <div className='relative'>
+        <div className='space-y-24'>
+          <div className='artwork-section mt-8 items-start gap-8 transition-all duration-1000 md:gap-12 lg:flex'>
+            <div className='w-full overflow-hidden lg:sticky lg:top-24 lg:w-1/2'>
               <Image
                 src={artwork.image}
                 alt={artwork.title}
-                height={800}
-                width={700}
-                className='animate-fade-in mx-auto max-h-[80vh] w-full max-w-2xl object-cover transition-transform duration-700 lg:mb-4'
+                height={850}
+                width={550}
+                className='animate-fade-in max-h-4xl mx-auto mb-12 h-full w-full max-w-4xl object-cover opacity-0 transition-transform duration-700 lg:mb-4'
               />
+              <p className='animate-fade-in mb-12 hidden text-center text-xl uppercase opacity-0 lg:block'>
+                {artwork.size} - {artwork.paintType}
+              </p>
             </div>
 
-            <div className='fixed inset-0 z-30 flex h-full w-full items-center justify-center'>
-              <div className='animate-fade-in inline-flex h-16 w-16 items-center justify-center rounded-lg bg-white/75 p-2 text-dark'>
-                <IoPlay className='mx-auto h-8 w-8' />
-              </div>
+            <div className='mx-auto lg:w-1/2'>
+              {artwork.description.map((desc, index) => (
+                <p
+                  key={index}
+                  className='animate-fade-in mt-0 text-lg text-gray-500 opacity-0'
+                >
+                  {desc}
+                </p>
+              ))}
+
+              <Image
+                src={artwork.subImage}
+                alt={artwork.title}
+                height={224}
+                width={550}
+                className='animate-fade-in mb-8 mt-16 max-h-40 w-full object-cover opacity-0'
+              />
+              <h4 className='animate-fade-in text-xl uppercase opacity-0'>
+                {artwork.subTitle}
+              </h4>
+              {artwork.subDescription.map((subDesc, index) => (
+                <p
+                  key={`subDescription-${index}`}
+                  className='animate-fade-in mt-0 text-lg text-gray-500 opacity-0'
+                >
+                  {subDesc}
+                </p>
+              ))}
             </div>
+          </div>
+          <p className='animate-fade-in mt-24 text-center text-xl font-medium uppercase opacity-0'>
+            {artwork.nextPaintingText}
+          </p>
+          <div className='mx-auto flex max-w-5xl justify-between'>
+            {currentIndex > 0 ? (
+              <Link
+                href={`/works/${artworks[currentIndex - 1].slug}`}
+                className='flex flex-col items-start gap-4 md:flex-row md:items-center md:gap-8'
+              >
+                <Image
+                  src={artworks[currentIndex - 1].image}
+                  alt={artworks[currentIndex - 1].title}
+                  height={100}
+                  width={150}
+                  className='h-[100px] w-[150px] object-cover md:order-2'
+                />
+                <span className='inline-flex items-center gap-2 text-left font-medium uppercase md:text-lg'>
+                  <LiaLongArrowAltLeftSolid />
+                  {artworks[currentIndex - 1].title}
+                </span>
+              </Link>
+            ) : (
+              <div className='ml-auto'></div>
+            )}
+            {currentIndex < artworks.length - 1 && (
+              <Link
+                href={`/works/${artworks[currentIndex + 1].slug}`}
+                className='flex flex-col items-end gap-4 md:flex-row md:items-center md:gap-8'
+              >
+                <Image
+                  src={artworks[currentIndex + 1].image}
+                  alt={artworks[currentIndex + 1].title}
+                  height={100}
+                  width={150}
+                  className='h-[100px] w-[150px] object-cover'
+                />
+                <span className='inline-flex items-center gap-2 text-right font-medium uppercase md:text-lg'>
+                  {artworks[currentIndex + 1].title}
+                  <LiaLongArrowAltRightSolid />
+                </span>
+              </Link>
+            )}
           </div>
         </div>
       </div>
-    );
-  };
-
-  return (
-    <Section ref={contentRef}>
-      {!isLoaded ? (
-        <Loader />
-      ) : (
-        <>
-          <AudioPlayer audioSrc={artwork.audioPath} />
-          <div className='text-center'>
-            <h2 className='animate-fade-in mb-0 break-words text-4xl uppercase opacity-0 md:text-7xl'>
-              {artwork.title}
-            </h2>
-            <h3 className='animate-fade-in taviraj text-3xl uppercase italic opacity-0 md:text-5xl'>
-              {artwork.frequency}
-            </h3>
-            <p className='animate-fade-in mb-0 block text-lg uppercase opacity-0 md:text-2xl lg:hidden'>
-              {artwork.size}
-              <br /> {artwork.paintType}
-            </p>
-          </div>
-
-          <div className='relative'>
-            <div className='space-y-24'>
-              <div className='artwork-section mt-8 items-start gap-8 transition-all duration-1000 md:gap-12 lg:flex'>
-                <div className='w-full overflow-hidden lg:sticky lg:top-24 lg:w-1/2'>
-                  <Image
-                    src={artwork.image}
-                    alt={artwork.title}
-                    height={850}
-                    width={550}
-                    className='animate-fade-in max-h-4xl mx-auto mb-12 h-full w-full max-w-4xl object-cover opacity-0 transition-transform duration-700 lg:mb-4'
-                  />
-                  <p className='animate-fade-in mb-12 hidden text-center text-xl uppercase opacity-0 lg:block'>
-                    {artwork.size} - {artwork.paintType}
-                  </p>
-                </div>
-
-                <div className='mx-auto lg:w-1/2'>
-                  {artwork.description.map((desc, index) => (
-                    <p
-                      key={index}
-                      className='animate-fade-in mt-0 text-lg text-gray-500 opacity-0'
-                    >
-                      {desc}
-                    </p>
-                  ))}
-
-                  <Image
-                    src={artwork.subImage}
-                    alt={artwork.title}
-                    height={224}
-                    width={550}
-                    className='animate-fade-in mb-8 mt-16 max-h-40 w-full object-cover opacity-0'
-                  />
-                  <h4 className='animate-fade-in text-xl uppercase opacity-0'>
-                    {artwork.subTitle}
-                  </h4>
-                  {artwork.subDescription.map((subDesc, index) => (
-                    <p
-                      key={`subDescription-${index}`}
-                      className='animate-fade-in mt-0 text-lg text-gray-500 opacity-0'
-                    >
-                      {subDesc}
-                    </p>
-                  ))}
-                </div>
-              </div>
-              <p className='animate-fade-in mt-24 text-center text-xl font-medium uppercase opacity-0'>
-                {artwork.nextPaintingText}
-              </p>
-              <div className='mx-auto flex max-w-5xl justify-between'>
-                {currentIndex > 0 ? (
-                  <Link
-                    href={`/works/${artworks[currentIndex - 1].slug}`}
-                    className='flex flex-col items-start gap-4 md:flex-row md:items-center md:gap-8'
-                  >
-                    <Image
-                      src={artworks[currentIndex - 1].image}
-                      alt={artworks[currentIndex - 1].title}
-                      height={100}
-                      width={150}
-                      className='h-[100px] w-[150px] object-cover md:order-2'
-                    />
-                    <span className='inline-flex items-center gap-2 text-left font-medium uppercase md:text-lg'>
-                      <LiaLongArrowAltLeftSolid />
-                      {artworks[currentIndex - 1].title}
-                    </span>
-                  </Link>
-                ) : (
-                  <div className='ml-auto'></div>
-                )}
-                {currentIndex < artworks.length - 1 && (
-                  <Link
-                    href={`/works/${artworks[currentIndex + 1].slug}`}
-                    className='flex flex-col items-end gap-4 md:flex-row md:items-center md:gap-8'
-                  >
-                    <Image
-                      src={artworks[currentIndex + 1].image}
-                      alt={artworks[currentIndex + 1].title}
-                      height={100}
-                      width={150}
-                      className='h-[100px] w-[150px] object-cover'
-                    />
-                    <span className='inline-flex items-center gap-2 text-right font-medium uppercase md:text-lg'>
-                      {artworks[currentIndex + 1].title}
-                      <LiaLongArrowAltRightSolid />
-                    </span>
-                  </Link>
-                )}
-              </div>
-            </div>
-          </div>
-        </>
-      )}
     </Section>
   );
 };
