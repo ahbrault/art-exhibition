b34fe1aab74c00ad92dee06803899fa5a9ab741b
Aurélien Brault <ahbrault@gmail.com>
2024-11-26 11:01:01 +0100

    Add section component for Works

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { artworks } from '@/data';
 import Link from 'next/link';
 import Image from 'next/image';
+import Section from '@/components/Section';
 
 interface VisibleSections {
   [key: string]: boolean;
@@ -78,60 +79,58 @@ const Works = () => {
   }, []);
 
   return (
-    <div className='relative min-h-screen bg-white text-dark'>
-      <div className='mx-auto max-w-6xl px-6 py-32 md:px-12' ref={contentRef}>
-        <h2 className='animate-fade-in mb-16 text-center opacity-0'>WORKS</h2>
+    <Section ref={contentRef} padding={false}>
+      <h2 className='animate-fade-in mb-16 text-center opacity-0'>WORKS</h2>
 
-        <div className='relative'>
-          {/* Barre de progression */}
-          <div className='absolute left-1/2 top-0 h-full w-px bg-gray-200'>
+      <div className='relative'>
+        {/* Barre de progression */}
+        <div className='absolute left-1/2 top-0 h-full w-px bg-gray-200'>
+          <div
+            className='w-px origin-top transform bg-dark transition-transform duration-100 ease-out'
+            style={{
+              height: '100%',
+              transform: `scaleY(${scrollProgress / 100})`,
+              willChange: 'transform',
+            }}
+          />
+        </div>
+
+        <div className='space-y-24'>
+          {artworks.map((artwork, index) => (
             <div
-              className='w-px origin-top transform bg-dark transition-transform duration-100 ease-out'
-              style={{
-                height: '100%',
-                transform: `scaleY(${scrollProgress / 100})`,
-                willChange: 'transform',
-              }}
-            />
-          </div>
-
-          <div className='space-y-24'>
-            {artworks.map((artwork, index) => (
-              <div
-                key={index}
-                id={`artwork-${index}`}
-                className={`artwork-section flex transform flex-row items-start gap-8 transition-all duration-1000 md:gap-24 ${
-                  visibleSections[`artwork-${index}`]
-                    ? 'translate-y-0 opacity-100'
-                    : 'translate-y-10 opacity-0'
-                }`}
-              >
-                <div className='w-1/2 overflow-hidden'>
-                  <Link href={`/works/${artwork.id}`}>
-                    <Image
-                      src={artwork.image}
-                      alt={artwork.title}
-                      layout='responsive'
-                      width={450}
-                      height={700}
-                      className='max-h-md ml-auto h-full w-full max-w-md object-cover transition-transform duration-700 hover:scale-110'
-                    />
-                  </Link>
-                </div>
-                <div className='w-1/2'>
-                  <Link href={`/works/${artwork.id}`}>
-                    <h3 className='mb-0 text-base font-medium uppercase sm:text-xl'>
-                      {artwork.title}
-                    </h3>
-                    <p className='mt-0 text-lg italic'>{artwork.frequency}</p>
-                  </Link>
-                </div>
+              key={index}
+              id={`artwork-${index}`}
+              className={`artwork-section flex transform flex-row items-start gap-8 transition-all duration-1000 md:gap-24 ${
+                visibleSections[`artwork-${index}`]
+                  ? 'translate-y-0 opacity-100'
+                  : 'translate-y-10 opacity-0'
+              }`}
+            >
+              <div className='w-1/2 overflow-hidden'>
+                <Link href={`/works/${artwork.id}`}>
+                  <Image
+                    src={artwork.image}
+                    alt={artwork.title}
+                    layout='responsive'
+                    width={450}
+                    height={700}
+                    className='max-h-md ml-auto h-full w-full max-w-md object-cover transition-transform duration-700 hover:scale-110'
+                  />
+                </Link>
               </div>
-            ))}
-          </div>
+              <div className='w-1/2'>
+                <Link href={`/works/${artwork.id}`}>
+                  <h3 className='mb-0 text-base font-medium uppercase sm:text-xl'>
+                    {artwork.title}
+                  </h3>
+                  <p className='mt-0 text-lg italic'>{artwork.frequency}</p>
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
